9d163d226bc4bb2642498cbf295e4c288a51f81f
ethan86116@gmail.com <ethan86116@gmail.com>
2017-02-15 21:14:46 +0800

    修正scheme與bug修正

--- models/account.js
+++ models/account.js
@@ -3,25 +3,29 @@ var Schema = mongoose.Schema;
 var passportMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
-	username: String,
-	password: String,
-	school: String,
-	grade: Number,
-	type: String,
-	vegan: Boolean,
-	mSpecial: String,
-	sSpecial: String,
-	size: String,
-	tel: String,
-	facebook: String,
-	email: String,
-	emergencyContact:String,
-	emergencyTel:String,
-	selfIntro: String,
-	mot: String,
-	demand: String,
-	contactUs: String,
-	howToKnowUs: Array
+    username: String, //姓名
+    gender: Boolean,  //性別
+    birthday: String, //生日
+    password: String, //身份證字號
+    bloodType: String,//血型
+    school: String,   //學校
+    grade: Number,    //年級
+    type: String,     //類組
+    vegan: Boolean,   //葷素
+    mSpecial: String, //特殊飲食習慣
+    sSpecial: String, //特殊疾病
+    size: String,     //營服尺寸
+    tel: String,      //聯絡電話
+    facebook: String, //FB
+    email: String,    //email
+    emergencyContact: String, //緊急聯絡人
+    emergenctRel: String,     //關係
+    emergencyTel: String,     //電話
+    selfIntro: String,        //自介
+    mot: String,              //動機
+    demand: String,           //期許
+    contactUs: String,        //想說的話
+    howToKnowUs: Array        //如何得知本營隊
 });
 
 Account.plugin(passportMongoose);
--- routes.js
+++ routes.js
@@ -1,26 +1,32 @@
 var passport = require('passport');
 var Account = require('./models/account');
 
-module.exports = function(app){
-	app.post('/register',function(req,res){
-    var password = req.body.password;
-    delete req.body.password;
-    console.log(req.body);
-		Account.register(new Account(req.body),password,function(err,account){
-			if(err){
-				console.log(err);
-				res.send('FAILED!');
-			}
-			else{
-				console.log('Created successfully.');
-				res.send('Created successfully.');
-			}
-		});
-	});
+module.exports = function(app) {
+    //IMPORTANT!!!!! UNSAFE CODE
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    });
+    //REMOVE ABOVE BEFORE PRODUCTION
+    app.post('/register', function(req, res) {
+        var password = req.body.password;
+        delete req.body.password;
+        console.log(req);
+        Account.register(new Account(req.body), password, function(err, account) {
+            if (err) {
+                console.log(err);
+                res.send('FAILED!');
+            } else {
+                console.log('Created successfully.');
+                res.send('Created successfully.');
+            }
+        });
+    });
 
 
-	app.get('/list',function(req,res){
+    app.get('/list', function(req, res) {
 
-	});
+    });
 
 };
